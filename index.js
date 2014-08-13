96c6e6765f486745e516814510d07101b8237551
Matt McKegg <matt@wetsand.co.nz>
2014-08-14 03:40:04 +1200

    mover, selector, suppressor, some refactoring

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var LoopGrid = require('loop-grid')
-var MidiGrid = require('midi-grid')
 var Switcher = require('switch-stream')
-var Controller = require('midi-controller')
-var ObserverStack = require('observer-stack')
-var ArrayGrid = require('array-grid')
-var ObservGrid = require('observ-grid')
-var ObservGridStack = require('observ-grid-stack')
-var DittyGridStream = require('./lib/ditty-grid-stream')
+var Observ = require('observ')
+var ObservArray = require('observ-array')
 
-var stateLights = require('./lib/state_lights')
-var repeatStates = [1, 2/3, 1/2, 1/3, 1/4, 1/6, 1/8]
 var Repeater = require('./lib/repeater')
 var Holder = require('./lib/holder')
+var Selector = require('./lib/selector')
+var Mover = require('./lib/mover')
+var Suppressor = require('./lib/suppressor')
 
+var stateLights = require('./lib/state_lights')
+var ControllerGrid = require('./lib/controller-grid')
+var RepeatButtons = require('./lib/repeat-buttons')
+var ControlButtons = require('./lib/control-buttons')
 
 module.exports = function Launchpad(opts){
 
@@ -22,129 +22,77 @@ module.exports = function Launchpad(opts){
   var duplexPort = Switcher()
   var triggerOutput = opts.triggerOutput
 
-  var self = LoopGrid(opts)
+  var self = LoopGrid(opts, {
+    repeatLength: Observ(2),
+    loopLength: Observ(8),
+    selection: ObservArray([]),
+    recording: ObservArray([]),
+    loopPosition: Observ()
+  })
 
-  var controller = Controller(duplexPort)
+  var lastPosition = -1
+  opts.scheduler.on('data', function(schedule){
+    if (Math.floor(schedule.from*10) > Math.floor(lastPosition*10)){
+      self.loopPosition.set(Math.floor(schedule.from) % self.loopLength())
+    }
+  })
 
-  // layers
-  var layers = {
-    recording: ObservGrid([], opts.shape),
-    active: ObservGrid([], opts.shape),
-    playing: ObservGrid([], opts.shape)
-  }
+  var buttons = ControlButtons(self, duplexPort)
+  var repeatButtons = RepeatButtons(self, duplexPort)
 
-  var outputStack = ObservGridStack([
-    layers.recording, layers.active, layers.playing
-  ])
+  var controllerGrid = ControllerGrid(self, {
+    duplexPort: duplexPort, 
+    triggerOutput: triggerOutput,
+    scheduler: opts.scheduler,
+    player: opts.player
+  })
 
-  var controllerGrid = MidiGrid(duplexPort, generateNoteGrid(), outputStack)
-  var inputStack = ObserverStack(controllerGrid)
+  var inputStack = controllerGrid.inputStack
+  var layers = controllerGrid.layers
 
   var repeater = Repeater(inputStack, self)
   var holder = Holder(self)
+  var selector = Selector(inputStack, self.selection, layers.selection, stateLights.green)
+  var mover = Mover(self, inputStack)
+  var suppressor = Suppressor(self, layers.suppressing, stateLights.red)
 
   self.setMidi = function(port){
     if (port){
       duplexPort.set(port)
       // clear lights
       duplexPort.write([176, 0, 0])
-      controllerGrid.resend()
+      duplexPort.emit('switch')
     }
-  }
-
-  var lastTriggeredAt = {}
-  triggerOutput.on('data', function(data){
-    var coords = self.grid().lookup(data.id)
-    lastTriggeredAt[data.id] = data.position
-    if (coords){
-      if (data.event === 'start'){
-        layers.playing.set(coords[0], coords[1], stateLights.amber)
-      } else if (data.event === 'stop'){
-        layers.playing.set(coords[0], coords[1], null)
-      }
-    }
-  })
-
-  // trigger notes at bottom of input stack
-  DittyGridStream(inputStack, self.grid, opts.scheduler).pipe(triggerOutput)
-
-  var playing = {}
-  opts.player.on('change', function(descriptor){
-    var value = descriptor.events && descriptor.events.length && true || null
-    if (value != descriptor.id){
-      playing[descriptor.id] = value
-      refreshPlaying()
-    }
-  })
-
-  self.grid(refreshPlaying)
-  self.grid(refreshRecording)
-  opts.scheduler.on('data', checkRecording)
-
-  var recording = {}
-  function checkRecording(schedule){
-    var changed = false
-    Object.keys(lastTriggeredAt).forEach(function(key){
-      var value = (lastTriggeredAt[key] > schedule.to - 8)
-      if (value != recording[key]){
-        recording[key] = value
-        changed = true
-      }
-    })
-    if (changed){
-      refreshRecording()
-    }
-  }
-
-  function refreshRecording(){
-    console.log('refreshing recording', recording)
-    gridForEach(self.grid(), function(val, row, col){
-      var currentState = layers.recording.get(row, col)
-      var value = recording[val]
-      if (value && currentState != stateLights.redLow){
-        layers.recording.set(row, col, stateLights.redLow)
-      } else if (!value && currentState != null){
-        layers.recording.set(row, col, null)
-      }
-    })
-  }
-
-  function refreshPlaying(){
-    gridForEach(self.grid(), function(val, row, col){
-      var currentState = layers.active.get(row, col)
-      var value = playing[val]
-      if (value && currentState != stateLights.greenLow){
-        layers.active.set(row, col, stateLights.greenLow)
-      } else if (!value && currentState != null){
-        layers.active.set(row, col, null)
-      }
-    })
   }
 
   self.setMidi(opts.midi)
 
   var learnMode = 'store'
   var recordingNotes = []
+
   function refreshLearnButton(){
     if (self.transforms.getLength()){
-      learnButton.setOff(stateLights.greenLow)
+      buttons.loopRange.state.set(stateLights.greenLow)
       learnMode = 'flatten'
     } else {
       if (recordingNotes.length > 0){
-        learnButton.setOff(stateLights.redLow)
+        buttons.loopRange.state.set(stateLights.redLow)
       } else {
-        learnButton.setOff(stateLights.off)
+        buttons.loopRange.state.set(stateLights.off)
       }
       learnMode = 'store'
     }
   }
 
-  var learnButton = controller.createButton([176, 104], function(){
-    this.flash(stateLights.green)
-    if (learnMode === 'store'){
-      self.loopRange(opts.scheduler.getCurrentPosition()-8, 8)
-    } else if (learnMode === 'flatten'){
-      self.flatten()
+  buttons.loopRange(function(value){
+    if (value){
+      buttons.loopRange.flash(stateLights.green)
+      if (learnMode === 'store'){
+        self.loopRange(opts.scheduler.getCurrentPosition()-self.loopLength(), self.loopLength())
+      } else if (learnMode === 'flatten'){
+        self.flatten()
+        clearSelection()
+      }
     }
   })
 
@@ -152,120 +100,76 @@ module.exports = function Launchpad(opts){
     refreshLearnButton()
   })
 
-  var undoButton = controller.createButton([176, 106], function(){
-    this.flash(stateLights.red, 100)
-    self.undo()
-  })
-  undoButton.setOff(stateLights.redLow)
-
-  var redoButton = controller.createButton([176, 107], function(){
-    this.flash(stateLights.red, 100)
-    self.redo()
-  })
-  redoButton.setOff(stateLights.redLow)
-
-  var holdButton = controller.createButton([176, 108], function(){
-    this.turnOn(stateLights.yellow)
-    holder.start(opts.scheduler.getCurrentPosition())
-  }, function(){
-    this.turnOff()
-    holder.stop()
+  buttons.undo.output.set(stateLights.redLow)
+  buttons.undo(function(value){
+    if (value){
+      buttons.undo.flash(stateLights.red, 100)
+      self.undo()
+    }
   })
 
-  var releaseSuppress = null
-  var suppressButton = controller.createButton([176, 105], function(){
-    this.turnOn(stateLights.red)
-    releaseSuppress = self.transform(function(input){
-      input.data.forEach(function(loop){
-        if (loop){
-          loop.events = []
-        }
-      })
-      return input
-    })
-  }, function(){
-    this.turnOff()
-    releaseSuppress()
+  buttons.redo.output.set(stateLights.redLow)
+  buttons.redo(function(value){
+    if (value){
+      buttons.redo.flash(stateLights.red, 100)
+      self.redo()
+    }
   })
 
-  var clearRepeatButton = controller.createButton([144, 8], function(){
-    clearRepeat()
-    this.turnOn(stateLights.amberLow)
-    repeater.stop()
+  buttons.hold(function(value){
+    if (value){
+      buttons.hold.output.set(stateLights.yellow)
+      holder.start(opts.scheduler.getCurrentPosition(), selector.selection())
+    } else {
+      buttons.hold.output.set(stateLights.off)
+      holder.stop()
+    }
   })
 
-  function clearRepeat(){
-    holder.setLength(2/1)
-    clearRepeatButton.turnOff()
-    repeatButtons.forEach(function(button){
-      button.turnOff()
-    })
+  buttons.suppress(function(value){
+    if (value){
+      buttons.suppress.output.set(stateLights.red)
+      suppressor.start(selector.selection())
+    } else {
+      buttons.suppress.output.set(stateLights.off)
+      suppressor.stop()
+    }
+  })
+
+  function clearSelection(){
+    mover.stop()
+    selector.clear()
+    selector.stop()
+    buttons.select.output.set(stateLights.off)
   }
 
-  clearRepeatButton.turnOn(stateLights.amberLow)
-
-  var repeatButtons = repeatStates.map(function(length, i){
-    var id = 8 + (i*16)
-    return controller.createButton([144, id+16], function(){
-      clearRepeat()
-      this.turnOn(stateLights.amberLow)
-      repeater.start(length)
-      holder.setLength(length)
-    })
+  buttons.select(function(value){
+    if (value){
+      mover.stop()
+      selector.clear()
+      buttons.select.output.set(stateLights.green)
+      selector.start()
+    } else {
+      if (selector.selection.getLength()){
+        mover.start(selector.selection())
+      } else {
+        selector.stop()
+        buttons.select.output.set(stateLights.off)
+      }
+    }
   })
 
-  var sideButtons = [clearRepeatButton].concat(repeatButtons)
+  self.repeatLength(function(value){
 
-  // handle lighting up notes
+    if (value < 2){
+      repeater.start()
+    } else {
+      repeater.stop()
+    }
 
-  // play notes
-
-  // repeat modes
-
-  // learn, suppress, undo, redo, hold
-
-  // move
+    repeater.setLength(value)
+    holder.setLength(value)
+  })
 
   return self
-
-  ///
-
-
-  function getGlobalIdFromMidi(noteId){
-    var row = Math.floor(noteId / 8)
-    var col = noteId % 8
-    return self.grid().get(row, col)
-  }
-
-  function getMidiFromGlobalId(globalId){
-    var coords = self.grid().lookup(globalId)
-    if (coords){
-      return (coords[0] * 8) + (coords[1] % 8)
-    }
-  }
-
-}
-
-
-
-function generateNoteGrid(message){
-  var result = []
-  var message = message || 144
-
-  for (var r=0;r<8;r++){
-    for (var c=0;c<8;c++){
-      var noteId = (r*16) + (c % 8)
-      result.push(message + '/' + noteId)
-    }
-  }
-
-  return ArrayGrid(result, [8,8]) 
-}
-
-function gridForEach(grid, iterator){
-  for (var r=0;r<grid.shape[0];r++){
-    for (var c=0;c<grid.shape[1];c++){
-      iterator(grid.get(r,c), r, c)
-    }
-  }
 }
