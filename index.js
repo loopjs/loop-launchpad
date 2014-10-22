ee309c8c2bed7c7cb93e6036e43eacfb70db7b2a
Matt McKegg <matt@wetsand.co.nz>
2014-10-23 02:47:23 +1300

    make gridMapping clearer

--- index.js
+++ index.js
@@ -250,11 +250,10 @@ module.exports = function(opts){
 
 function getLaunchpadGridMapping(){
   var result = []
-  var message = 144
   for (var r=0;r<8;r++){
     for (var c=0;c<8;c++){
       var noteId = (r*16) + (c % 8)
-      result.push(message + '/' + noteId)
+      result.push('144/' + noteId)
     }
   }
   return ArrayGrid(result, [8, 8])
